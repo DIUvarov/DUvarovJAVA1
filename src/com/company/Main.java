e79b89f88cd2d996f419540b5dd3b745001e36e0
Uvarov Dmitry <dimarabochi@gmail.com>
2021-10-03 18:29:14 +0300

    Задача 6

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,36 +4,91 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        //Задача 5
-          // 1. Ввести 2 числа с клавиатуры, затем один из символов ‘+’, ‘-’, ‘*’ или ‘/’.
-          // 2. зависимости от символа вывести в консоль результат выполнения соответствующей арифметической операции.
-        //пункт 1
-        Scanner scannerForCals = new Scanner(System.in);
-        double  numFirst = scannerForCals.nextInt();
-        String symbol = scannerForCals.next();
-        double  numSecond = scannerForCals.nextInt();
+        //Задача 6
+        Scanner scannerConv = new Scanner(System.in);
 
-        //пункт 2
-        switch (symbol) {
-            case "+":
-                System.out.println(numFirst + numSecond);
-                break;
-            case "-":
-                System.out.println(numFirst - numSecond);
-                break;
-            case "*":
-                System.out.println(numFirst * numSecond);
-                break;
-            case "/":
-                if (numSecond == 0) {
-                    System.out.println("На ноль делить нельзя");
-                } else {
-                    System.out.println(numFirst / numSecond);
+        int physicalQuantity, unit;
+        double number;
+
+        System.out.println("Выберите что переводить: 1 - масса, 2 - расстояние");
+        physicalQuantity = scannerConv.nextInt();
+
+        if (physicalQuantity == 1) {
+            System.out.println("Выберите единицу измерения: 1 - килограмм, 2 - стон, 3 - фунт, 4 - унция");
+            unit = scannerConv.nextInt();
+
+            if (unit >= 1 && unit <= 4) {
+                System.out.println("Введите число");
+                number = scannerConv.nextDouble();
+
+                switch (unit) {
+                    case 1:
+                        System.out.println("Килограмм: " + number);
+                        System.out.println("Стон: " + number / 6.35);
+                        System.out.println("Фунт: " + number * 2.205);
+                        System.out.println("Унция: " + number * 35.274);
+                        break;
+                    case 2:
+                        System.out.println("Стон: " + number);
+                        System.out.println("Килограмм: " + number * 6.35);
+                        System.out.println("Фунт: " + number * 14);
+                        System.out.println("Унция: " + number * 224);
+                        break;
+                    case 3:
+                        System.out.println("Фунт: " + number);
+                        System.out.println("Килограмм: " + number / 2.205);
+                        System.out.println("Стон: " + number / 14);
+                        System.out.println("Унция: " + number * 16);
+                        break;
+                    case 4:
+                        System.out.println("Унция: " + number);
+                        System.out.println("Килограмм: " + number / 35.274);
+                        System.out.println("Фунт: " + number / 16);
+                        System.out.println("Стон: " + number / 224);
+                        break;
                 }
-                break;
-            default:
-                System.out.println("Невыполнимая задача");
-        }
-        }
-}
+            } else {
+                System.out.println("Нужно выбрать от 1 до 4");
+            }
+        } else if (physicalQuantity == 2) {
+            System.out.println("Выберите единицу измерения: 1 - метр, 2 - миля, 3 - ярд, 4 - фут");
+            unit = scannerConv.nextInt();
 
+            if (unit >= 1 && unit <= 4) {
+                System.out.println("Введите число");
+                number = scannerConv.nextDouble();
+
+                switch (unit) {
+                    case 1:
+                        System.out.println("Метры: " + number);
+                        System.out.println("Мили: " + number / 1609);
+                        System.out.println("Ярды: " + number * 1.094);
+                        System.out.println("Футы: " + number * 3.281);
+                        break;
+                    case 2:
+                        System.out.println("Мили: " + number);
+                        System.out.println("Метры: " + number * 1609);
+                        System.out.println("Ярды: " + number * 1760);
+                        System.out.println("Футы: " + number * 5280);
+                        break;
+                    case 3:
+                        System.out.println("Ярды: " + number);
+                        System.out.println("Метры: " + number / 1.094);
+                        System.out.println("Мили: " + number / 1760);
+                        System.out.println("Футы: " + number * 3);
+                        break;
+                    case 4:
+                        System.out.println("Футы: " + number);
+                        System.out.println("Метры: " + number / 3.281);
+                        System.out.println("Мили: " + number / 5280);
+                        System.out.println("Ярды: " + number / 3);
+                        break;
+                }
+            } else {
+                System.out.println("Нужно выбрать от 1 до 4");
+            }
+        } else {
+            System.out.println("Нужно выбрать 1 или 2");
+        }
+    }
+}
